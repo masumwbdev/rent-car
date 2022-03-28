55605d1e72d46956dcadddabef6039f23c256432
masumwbdev <masumwbd@gmail.com>
2022-03-28 19:20:38 +0600

    displayed single service

--- src/Pages/Booking/Booking.js
+++ src/Pages/Booking/Booking.js
@@ -18,10 +18,43 @@ const Booking = () => {
         <div>
             <TopNav />
             <Navbar />
-            <h1>This is booking</h1>
-            <h1>Booking id {booking._id}</h1>
-            <h1>{booking.carName}</h1>
-            <img src={booking.imgURL} alt="" />
+            <div className="width-control">
+                <h4 className='mt-3 mb-3'>Service overview</h4>
+                <div className="row">
+                    <div className="col-sm-12 col-md-6">
+                        <img src={booking.imgURL} alt="" />
+                        <h5>{booking.carName}</h5>
+                        <ul>
+                            <li>
+                                {booking.passengers} Passengers
+                            </li>
+                            <li>
+                                {booking.largeSuitcases} Large suitcases, {booking.smallSuitcases} Small suitcases
+                            </li>
+                            <li>
+                                {booking.transmission} Transmission
+                            </li>
+                            <li>
+                                {booking.aircondition}
+                            </li>
+                            <li>
+                                {booking.oil} km/liters
+                            </li>
+                            <li>
+                                {booking.emmissions} CO2 emmissions
+                            </li>
+                            <li>
+                                {booking.regularCost}$
+                            </li>
+                        </ul>
+                        <h4>Car description</h4>
+                        <p>{booking.description}</p>
+                    </div>
+                    <div className="col-sm-12 col-md-6">
+
+                    </div>
+                </div>
+            </div>
             <TopFooter />
             <BottomFooter />
         </div>
