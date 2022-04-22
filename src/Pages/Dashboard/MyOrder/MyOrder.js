04be79bd43a3c92827cc0a6d602fe184bc3e1c3a
masumwbdev <masumwbd@gmail.com>
2022-04-23 00:08:16 +0600

    fixed remaining count after deleting

--- src/Pages/Dashboard/MyOrder/MyOrder.js
+++ src/Pages/Dashboard/MyOrder/MyOrder.js
@@ -1,25 +1,9 @@
 import React from 'react';
 import './MyOrder.css'
 
-const MyOrder = ({ myBook, mySetBooking }) => {
+const MyOrder = ({ myBook, handleDlt }) => {
     const { regularCost, img, title } = myBook;
-    const handleDlt = id => {
-        console.log(
-            'clicked'
-        )
-        const url = `http://localhost:5000/bookings/${id}`
-        fetch(url, {
-            method: 'DELETE'
-        })
-        .then(res => res.json())
-        .then(data => {
-            if(data.deletedCount > 0){
-                alert('Deleted successfully');
-                const remaining = myBook.filter(book => book._id !== id);
-                mySetBooking(remaining);
-            }
-        })
-    }
+    
     return (
         <div className='my-order width-control'>
             <div className='row'>
--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -13,11 +13,29 @@ const MyOrders = () => {
             .then(res => res.json())
             .then(data => mySetBooking(data));
     }, [user.email])
+    const handleDlt = id => {
+        console.log(
+            'clicked'
+        )
+        const url = `http://localhost:5000/bookings/${id}`
+        fetch(url, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.deletedCount > 0){
+                alert('Deleted successfully');
+                const remaining = myBooking.filter(book => book._id !== id);
+                mySetBooking(remaining);
+            }
+        })
+    }
     return (
         <div>
             {
                 myBooking.map(myBook => <MyOrder
                     myBook={myBook}
+                    handleDlt={handleDlt}
                 ></MyOrder>)
             }
         </div>
